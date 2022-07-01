2f090f4ccc5682e34d2228718c42bba43a6a8ac5
web3 vicky <vicky.web3@gmail.com>
2022-07-01 14:00:03 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let num1 = 5
+let num2 = 18
+
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+
+function add(){
+	let add = num1 + num2
+	document.getElementById("sum-el").textContent = "SUM: " + add
+}
+
+function subtract(){
+	let sub = num1 - num2
+	document.getElementById("sum-el").textContent = "SUM: " + sub
+}
+
+function divide(){
+	let div = num1 / num2
+	document.getElementById("sum-el").textContent = "SUM: " + div
+}
+
+function multiply(){
+	let mul = num1 * num2
+	document.getElementById("sum-el").textContent = "SUM: " + mul
+}
+
+
+
+
+
+
