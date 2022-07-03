fb85cb00fe3b96add4db96f0528986f6a4d44011
web3 vicky <vicky.web3@gmail.com>
2022-07-03 17:36:26 +0530

    first commit

--- calculator.js
+++ calculator.js
@@ -1,9 +1,10 @@
-let num1 = 5
+let num1 = 23
 let num2 = 18
 
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
+/* Several function for performing addition, subtraction, division and multiplication */
 function add(){
 	let add = num1 + num2
 	document.getElementById("sum-el").textContent = "SUM: " + add
